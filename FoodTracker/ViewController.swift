b2d28e5e613f056eaa85a2749233c02b1b64351e
trevor aukim <trevor@thinkemotive.com>
2018-01-26 14:04:59 +0000

    inital commit

---
+++ FoodTracker/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  FoodTracker
+//
+//  Created by trevor aukim on 05/11/2017.
+//  Copyright © 2017 trevor aukim. All rights reserved.
+//
+
+import UIKit
+//import AppCenterDistribute
+
+class ViewController: UIViewController {
+    
+    //MARK: Properties
+
+    @IBOutlet weak var mealNameLabel: UILabel!
+    @IBOutlet weak var nameTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+//        MSDistribute.setDelegate(self);
+        
+       }
+}
+
